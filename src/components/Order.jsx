6486ea6c9e79dfa892bf5e895e65597f5e4d4d44
Haktan Kocoglu <hktnkocoglu@gmail.com>
2025-09-23 23:48:56 +0300

    Order container konumları Order.css dosyasında düzenlendi

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -18,43 +18,60 @@ function Order() {
           </nav>
         </Router>
       </header>
-      <div className="order-header">
-        <h2>Position Absolute Acı Pizza</h2>
-        <p>
-          Frontend Dev olarka hala position:absolute kullanıyorsan bu çok acı
-          pizza tam sana göre. Pizza domates, peynir ve genellikle çeşitli
-          malzemelerle kaplanmış, daha sonra geleneksel olarka odun ateşinde bir
-          fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak,
-          düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli bir
-          lezzetli bir yemektir.
-        </p>
-      </div>
-      <div className="order-container">
-        <form>
-          <label>
-            Boyut Seç <br/>
-            <input type="radio" name="secim" value="kucuk" />
-            Küçük
-          </label>
-          <br />
-          <label>
-            <input type="radio" name="secim" value="orta" />
-            Orta
-          </label>
-          <br />
-          <label>
-            <input type="radio" name="secim" value="buyuk" />
-            Büyük
-          </label>
-          <br />
-          <label>
-            Hamur Seç:
-            <select name="hamur">
-              <option value="ince">İnce</option>
-              <option value="kalin">Kalın</option>
-            </select>
-          </label>
-        </form>
+      <div className="order-pages">
+        <div className="order-header">
+          <h2>Position Absolute Acı Pizza</h2>
+          <p>
+            Frontend Dev olarka hala position:absolute kullanıyorsan bu çok acı
+            pizza tam sana göre. Pizza domates, peynir ve genellikle çeşitli
+            malzemelerle kaplanmış, daha sonra geleneksel olarka odun ateşinde
+            bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak,
+            düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli
+            bir lezzetli bir yemektir.
+          </p>
+
+          <form style={{ display: "flex" }}>
+            <div
+              style={{
+                width: "50%",
+                display: "inline-block",
+              }}
+            >
+              <label>
+                Boyut Seç <br />
+                <input type="radio" name="secim" value="kucuk" />
+                Küçük
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="secim" value="orta" />
+                Orta
+              </label>
+              <br />
+              <label>
+                <input type="radio" name="secim" value="buyuk" />
+                Büyük
+              </label>
+            </div>
+            <div
+              style={{
+                width: "50%",
+                display: "inline-block",
+              }}
+            >
+              <label>
+                Hamur Seç: <br />
+                <select name="hamur" defaultValue="">
+                  <option value="" disabled>
+                    Hamur Kalınlığı
+                  </option>
+                  <option value="ince">İnce</option>
+                  <option value="kalin">Kalın</option>
+                </select>
+              </label>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
